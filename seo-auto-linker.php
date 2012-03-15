908c289aa22b9637433930fa5b35ff48386fcbd1
chrisguitarguy <chrisguitarguy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-15 18:16:35 +0000

    Fixes several issue and tagging a new version
    git-svn-id: https://plugins.svn.wordpress.org/seo-auto-linker/trunk@519471 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- seo-auto-linker.php
+++ seo-auto-linker.php
@@ -3,9 +3,9 @@
 Plugin Name: SEO Auto Linker
 Plugin URI: http://pmg.co/seo-auto-linker
 Description: Allows you to automatically link terms with in your post, page or custom post type content.
-Version: 0.5
+Version: 0.6
 Author: Christopher Davis at PMG
-Author URI: http://pmg.co/people/chris
+Author URI: http://christopherdavis.me
 */
 
 define( 'SEOAL_PATH', plugin_dir_path( __FILE__ ) );
@@ -13,9 +13,9 @@ define( 'SEOAL_URL', plugin_dir_url( __FILE__ ) );
 
 if( is_admin() )
 {
-	require_once( SEOAL_PATH . 'inc/admin.php' );
+    require_once( SEOAL_PATH . 'inc/admin.php' );
 }
 else
 {
-	require_once( SEOAL_PATH . 'inc/front.php' );
+    require_once( SEOAL_PATH . 'inc/front.php' );
 }
